72199896e4de922c4dc810be914f7eed1e738fcd
Kateryna <kunieva.kateryna@gmail.com>
2023-06-08 07:05:12 +0300

    Fixed container

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php include_once('header.php') ?>
 <main>
-      <div class="main_start">
+      <div class="main_head">
         <div class="container">
           <div class="main__content">
             <h2 class="main__title">
-              Открой для себя рай <br />
-              жизни на острове Кипр
+              Открой для себя рай жизни на острове Кипр
             </h2>
             <button class="main__btn">Подобрать недвижимость</button>
           </div>
@@ -13,10 +12,9 @@
       </div>
 
       <div class="about">
-        <div class="container">
-          <div class="about__content">
+        <div class="container"> 
             <h2 class="about__title">
-              Почему <br />
+              Почему 
               <span>THE Family Estate</span>?
             </h2>
             <div class="about__list">
@@ -78,8 +76,7 @@
                   Построенных квадратных метров
                 </div>
               </div>
-            </div>
-          </div>
+            </div> 
         </div>
       </div>
 
@@ -88,75 +85,76 @@
           <div class="advantages__title">
             приемущества <span>Северного Кипра</span>
           </div>
-        </div>
-        <div class="advantages__list">
-          <div class="advantages__list_item">
-            <div class="advantages__list-logo">
-              <img src="img/advantages/1.png" alt="" />
+        
+          <div class="advantages__list">
+            <div class="advantages__list_item">
+              <div class="advantages__list-logo">
+                <img src="img/advantages/1.png" alt="" />
+              </div>
+              <div class="advantages__list-text">
+                Более 300 солнечных дней в году
+              </div>
             </div>
-            <div class="advantages__list-text">
-              Более 300 солнечных дней в году
+            <div class="advantages__list_item">
+              <div class="advantages__list-logo">
+                <img src="img/advantages/2.png" alt="" />
+              </div>
+              <div class="advantages__list-text">
+                Мягкий морской климат и чистый воздух
+              </div>
             </div>
-          </div>
-          <div class="advantages__list_item">
-            <div class="advantages__list-logo">
-              <img src="img/advantages/2.png" alt="" />
+            <div class="advantages__list_item">
+              <div class="advantages__list-logo">
+                <img src="img/advantages/3.png" alt="" />
+              </div>
+              <div class="advantages__list-text">
+                Международная система образования
+              </div>
             </div>
-            <div class="advantages__list-text">
-              Мягкий морской климат и чистый воздух
+            <div class="advantages__list_item">
+              <div class="advantages__list-logo">
+                <img src="img/advantages/4.png" alt="" />
+              </div>
+              <div class="advantages__list-text">
+                Органические продукты с местных плантаций
+              </div>
             </div>
-          </div>
-          <div class="advantages__list_item">
-            <div class="advantages__list-logo">
-              <img src="img/advantages/3.png" alt="" />
+            <div class="advantages__list_item">
+              <div class="advantages__list-logo">
+                <img src="img/advantages/5.png" alt="" />
+              </div>
+              <div class="advantages__list-text">
+                Отсутствие запроса на происхождение денежных средств
+              </div>
             </div>
-            <div class="advantages__list-text">
-              Международная система образования
+            <div class="advantages__list_item">
+              <div class="advantages__list-logo">
+                <img src="img/advantages/6.png" alt="" />
+              </div>
+              <div class="advantages__list-text">
+                Возможность расчётов в криптовалюте
+              </div>
             </div>
-          </div>
-          <div class="advantages__list_item">
-            <div class="advantages__list-logo">
-              <img src="img/advantages/4.png" alt="" />
+            <div class="advantages__list_item">
+              <div class="advantages__list-logo">
+                <img src="img/advantages/7.png" alt="" />
+              </div>
+              <div class="advantages__list-text">Отсутствие преступности</div>
             </div>
-            <div class="advantages__list-text">
-              Органические продукты с местных плантаций
-            </div>
-          </div>
-          <div class="advantages__list_item">
-            <div class="advantages__list-logo">
-              <img src="img/advantages/5.png" alt="" />
-            </div>
-            <div class="advantages__list-text">
-              Отсутствие запроса на происхождение денежных средств
-            </div>
-          </div>
-          <div class="advantages__list_item">
-            <div class="advantages__list-logo">
-              <img src="img/advantages/6.png" alt="" />
-            </div>
-            <div class="advantages__list-text">
-              Возможность расчётов в криптовалюте
-            </div>
-          </div>
-          <div class="advantages__list_item">
-            <div class="advantages__list-logo">
-              <img src="img/advantages/7.png" alt="" />
-            </div>
-            <div class="advantages__list-text">Отсутствие преступности</div>
-          </div>
-          <div class="advantages__list_item">
-            <div class="advantages__list-logo">
-              <img src="img/advantages/8.png" alt="" />
-            </div>
-            <div class="advantages__list-text">
-              Более 300 солнечных дней в году
+            <div class="advantages__list_item">
+              <div class="advantages__list-logo">
+                <img src="img/advantages/8.png" alt="" />
+              </div>
+              <div class="advantages__list-text">
+                Более 300 солнечных дней в году
+              </div>
             </div>
           </div>
         </div>
       </div>
 
       <div class="search">
-        <div class="search__inner">
+        <div class="container">
           <div class="search__criteria">
             <div class="search__title">
               ВЫБЕРИТЕ <span>ПАРАМЕТРЫ ПОИСКА</span>
@@ -214,15 +212,6 @@
             <div class="search__cards_item">
               <div class="search__cards_img">
                 <a href=""><img src="img/list_cards/5.png" alt="" /></a>
-              </div>
-              <div class="search__cards_text">
-                <div class="search__cards_title">Palm Homes</div>
-                <div class="search__cards_price">от 240 000$</div>
-              </div>
-            </div>
-            <div class="search__cards_item">
-              <div class="search__cards_img">
-                <a href=""><img src="img/list_cards/6.png" alt="" /></a>
               </div>
               <div class="search__cards_text">
                 <div class="search__cards_title">Palm Homes</div>
